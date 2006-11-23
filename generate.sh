121e001053727246ab3e72c981f263a050bf0018
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2006-11-23 23:35:58 +0000

    Less output -- only use new line and print graph name if graphviz crashes on it.
    git-svn-id: http://canviz.googlecode.com/svn/canviz/trunk@36 96afbf3d-d051-0410-af9b-0f47d9322047

--- generate.sh
+++ generate.sh
@@ -8,10 +8,11 @@ OUTPUT_GRAPHS=./graphs
 
 echo "Generating with $DOT_VERSION"
 for PROGRAM in dot neato fdp circo twopi; do
+	printf '%-8s' "$PROGRAM:"
 	mkdir -p $OUTPUT_GRAPHS/$PROGRAM
 	for FILE_PATH in $INSTALL_DIR/share/graphviz/graphs/directed/*.dot $INSTALL_DIR/share/graphviz/graphs/undirected/*.dot; do
 		FILE_NAME=`basename $FILE_PATH`
-		echo -n $PROGRAM -Txdot $FILE_NAME
+		echo -n "."
 		(time $INSTALL_DIR/bin/$PROGRAM -Txdot $FILE_PATH > $TEMP_GRAPH) 2> $TEMP_HEADER
 		if [ -s $TEMP_GRAPH ]; then
 			NOW=`TZ=GMT date`
@@ -20,10 +21,11 @@ for PROGRAM in dot neato fdp circo twopi; do
 			sed 's/^/# /' < $TEMP_HEADER >> $OUTPUT_GRAPHS/$PROGRAM/$FILE_NAME
 			echo >> $OUTPUT_GRAPHS/$PROGRAM/$FILE_NAME
 			cat $TEMP_GRAPH >> $OUTPUT_GRAPHS/$PROGRAM/$FILE_NAME
-			echo
 		else
-			echo ' - CRASHED!'
+			echo
+			echo "$FILE_NAME - CRASHED!"
 		fi
 	done
+	echo
 done
 rm -f $TEMP_GRAPH $TEMP_HEADER
