b6fb1d971048d7b57c65cdc4ad885d1a95a19779
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2006-11-24 06:02:53 +0000

    Compress the dot file (using bzip2, gzip or deflate) as it goes out over the wire, if the browser supports it.
    git-svn-id: http://canviz.googlecode.com/svn/canviz/trunk@37 96afbf3d-d051-0410-af9b-0f47d9322047

--- graph.php
+++ graph.php
@@ -8,6 +8,17 @@ if (!file_exists($file_path)) exit;
 
 $graph_src = file_get_contents($file_path);
 //$graph_src = shell_exec('/opt/local/bin/dot -Txdot /usr/local/graphviz/share/graphviz/graphs/directed/pmpipe.dot');
+
+require_once 'lib/AcceptEncoding.class.php';
+$encoding = new AcceptEncoding();
+if ($encoding->acceptable('bzip2') && function_exists('bzcompress')) {
+	header('Content-Encoding: bzip2');
+	header('Vary: Accept-Encoding');
+	ob_start('bzcompress');
+} else if ($encoding->acceptable('gzip') || $encoding->acceptable('deflate')) {
+	ob_start('ob_gzhandler');
+}
+
 header('Content-Type: text/plain');
 echo $graph_src;
 exit;
