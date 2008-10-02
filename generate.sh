04d5183206f53917b2fc4c586590c55193740591
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2008-10-02 08:00:43 +0000

    generate.sh: Use bash's $(...) syntax instead of sh's `...` syntax for executing programs; this will make it easier to do nested program launches.
    git-svn-id: http://canviz.googlecode.com/svn/canviz/trunk@82 96afbf3d-d051-0410-af9b-0f47d9322047

--- generate.sh
+++ generate.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 INSTALL_DIR=/usr/local/graphviz-devel
-TEMP_GRAPH=`mktemp -t canviz`
-TEMP_HEADER=`mktemp -t canviz`
-DOT_VERSION=`$INSTALL_DIR/bin/dot -V 2>&1`
+TEMP_GRAPH=$(mktemp -t canviz)
+TEMP_HEADER=$(mktemp -t canviz)
+DOT_VERSION=$($INSTALL_DIR/bin/dot -V 2>&1)
 OUTPUT_GRAPHS=./graphs
 
 echo "Generating with $DOT_VERSION"
@@ -11,11 +11,11 @@ for PROGRAM in dot neato fdp circo twopi; do
 	printf '%-8s' "$PROGRAM:"
 	mkdir -p $OUTPUT_GRAPHS/$PROGRAM
 	for FILE_PATH in $INSTALL_DIR/share/graphviz/graphs/directed/*.dot $INSTALL_DIR/share/graphviz/graphs/undirected/*.dot; do
-		FILE_NAME=`basename $FILE_PATH`
+		FILE_NAME=$(basename $FILE_PATH)
 		echo -n "."
 		(time $INSTALL_DIR/bin/$PROGRAM -Txdot $FILE_PATH > $TEMP_GRAPH) 2> $TEMP_HEADER
 		if [ -s $TEMP_GRAPH ]; then
-			NOW=`TZ=GMT date`
+			NOW=$(TZ=GMT date)
 			echo "# Generated $NOW by $DOT_VERSION" > $OUTPUT_GRAPHS/$PROGRAM/$FILE_NAME
 			echo '#' >> $OUTPUT_GRAPHS/$PROGRAM/$FILE_NAME
 			sed 's/^/# /' < $TEMP_HEADER >> $OUTPUT_GRAPHS/$PROGRAM/$FILE_NAME
