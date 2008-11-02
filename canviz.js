026b477ef706e801a7482dc8991c6bac1c8d4e8e
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2008-11-02 04:47:50 +0000

    canviz.js: Add debug message and a "broken image" graphic when an image can't be loaded. See issue #33.
    git-svn-id: http://canviz.googlecode.com/svn/canviz/trunk@204 96afbf3d-d051-0410-af9b-0f47d9322047

--- canviz.js
+++ canviz.js
@@ -659,7 +659,7 @@ var GraphImage = Class.create({
 		this.canviz = canviz;
 		++this.canviz.numImages;
 		this.src = this.canviz.imagePath + src;
-		this.loaded = false;
+		this.finished = this.loaded = false;
 		this.img = new Image();
 		this.img.onload = this.onLoad.bind(this);
 		this.img.onerror = this.onFinish.bind(this);
@@ -671,15 +671,34 @@ var GraphImage = Class.create({
 		this.onFinish();
 	},
 	onFinish: function() {
+		this.finished = true;
 		++this.canviz.numImagesFinished;
 		if (this.canviz.numImages == this.canviz.numImagesFinished) {
 			this.canviz.draw(true);
 		}
 	},
 	draw: function(ctx, l, t, w, h) {
-		if (this.loaded) {
-			ctx.drawImage(this.img, l, t, w, h);
+		if (this.finished) {
+			if (this.loaded) {
+				ctx.drawImage(this.img, l, t, w, h);
+			} else {
+				debug('can\'t load image ' + this.src);
+				this.drawBrokenImage(ctx, l, t, w, h);
+			}
 		}
+	},
+	drawBrokenImage: function(ctx, l, t, w, h) {
+		ctx.save();
+		ctx.beginPath();
+		new Rect(l, t, l + w, t + w).draw(ctx);
+		ctx.moveTo(l, t);
+		ctx.lineTo(l + w, t + w);
+		ctx.moveTo(l + w, t);
+		ctx.lineTo(l, t + h);
+		ctx.strokeStyle = '#f00';
+		ctx.lineWidth = 1;
+		ctx.stroke();
+		ctx.restore();
 	}
 });
 
