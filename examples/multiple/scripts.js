151dd8d70a67a7111738a104604e53f8575d6a96
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2012-02-07 03:45:31 +0000

    multiple: use contentLoaded
    git-svn-id: http://canviz.googlecode.com/svn/canviz/trunk@367 96afbf3d-d051-0410-af9b-0f47d9322047

--- examples/multiple/scripts.js
+++ examples/multiple/scripts.js
@@ -1,3 +1,7 @@
-new Canviz('graph1', 'graph1-xdot.gv');
-new Canviz('graph2', 'graph2-xdot.gv');
-new Canviz('graph3', 'graph3-xdot.gv');
+contentLoaded(window, function() {
+  setTimeout(function() {
+    new Canviz('graph1', 'graph1-xdot.gv');
+    new Canviz('graph2', 'graph2-xdot.gv');
+    new Canviz('graph3', 'graph3-xdot.gv');
+  }, 0);
+});
